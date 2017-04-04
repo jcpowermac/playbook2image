0ac67fc4e3f68fcc72877374bcea3434b61ba201
Joseph Callen <jcallen@redhat.com>
2017-04-04 19:51:20 -0400

    Changes from testing

--- jenkins/jobs/credPipelineJob.groovy
+++ jenkins/jobs/credPipelineJob.groovy
@@ -7,11 +7,13 @@ node {
     def id = null
     def utils = new Utils()
 
-    openshift.withCluster() {
-        def secret = openshift.selector( "secret/github" ).object()
-        id = utils.createCredentialsFromOpenShift(secret, "github") 
+    stage('OpenShift -> Jenkins credentials') {
+        openshift.withCluster() {
+            def secret = openshift.selector( "secret/github" ).object()
+            id = utils.createCredentialsFromOpenShift(secret, "github") 
+        }
     }
-                
-    println(id)
-    build job: 'seed', parameters: [[$class: 'StringParameterValue', name: 'CRED_ID', value: id]]
+    stage('Run Seed Job') {            
+        build job: 'seed', parameters: [[$class: 'StringParameterValue', name: 'CRED_ID', value: id]]
+    }
 }
